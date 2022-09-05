7f6d79c9acad240d4c2dce730498d1e421b29327
liver121888 <liver121888@gmail.com>
2022-09-05 17:57:37 +0800

    mv data, add launch.

--- catkin_ws/src/kitti_tutorial/script/kitti.py
+++ catkin_ws/src/kitti_tutorial/script/kitti.py
@@ -3,11 +3,46 @@
 import os
 from data_utils import *
 from publish_utils import *
+from kitti_util import *
+import rospkg
 
-DATA_PATH = '/home/liver/data/kitti/RawData/2011_09_26/2011_09_26_drive_0005_sync/'
-TRACKING_PATH = '/home/liver/data/kitti/Tracking/training/label_02/0000.txt'
+
+
+DATA_PATH = 'data/kitti/RawData/2011_09_26/2011_09_26_drive_0005_sync/'
+TRACKING_PATH = 'data/kitti/Tracking/training/label_02/0000.txt'
+CALIB_PATH = 'data/kitti/RawData/2011_09_26/'
+
+
+def compute_3d_box_cam2(h, w, l, x, y, z, yaw):
+    """
+    Return : 3xn in cam2 coordinate
+    """
+    R = np.array([[np.cos(yaw), 0, np.sin(yaw)], [0, 1, 0], [-np.sin(yaw), 0, np.cos(yaw)]])
+    x_corners = [l/2,l/2,-l/2,-l/2,l/2,l/2,-l/2,-l/2]
+    y_corners = [0,0,0,0,-h,-h,-h,-h]
+    z_corners = [w/2,-w/2,-w/2,w/2,w/2,-w/2,-w/2,w/2]
+    corners_3d_cam2 = np.dot(R, np.vstack([x_corners,y_corners,z_corners]))
+    #     corners_3d_cam2[0,:] += x
+    #     corners_3d_cam2[1,:] += y
+    #     corners_3d_cam2[2,:] += z
+    corners_3d_cam2 += np.vstack([x, y, z])
+    return corners_3d_cam2
+
+
 
 if __name__ == '__main__':
+    # get an instance of RosPack with the default search paths
+    rospack = rospkg.RosPack()
+
+    # list all packages, equivalent to rospack list
+    rospack.list() 
+
+    # get the file path for rospy_tutorials
+    pkg_path = rospack.get_path('kitti_tutorial')
+    print(pkg_path)
+    print(type(pkg_path))
+
+
     frame = 0
     rospy.init_node('kitti_node', anonymous=True)
     bridge = CvBridge()
@@ -16,19 +51,36 @@ if __name__ == '__main__':
     ego_pub = rospy.Publisher('kitti_ego_car', MarkerArray, queue_size=10)
     imu_pub = rospy.Publisher('kitti_imu', Imu, queue_size=10)
     gps_pub = rospy.Publisher('kitti_gps', NavSatFix, queue_size=10)
-    df_tracking = read_tracking(TRACKING_PATH)
+    box3d_pub = rospy.Publisher('kitti_3d', MarkerArray, queue_size=10)
+    print(os.path.join(pkg_path, TRACKING_PATH))
+    df_tracking = read_tracking(os.path.join(pkg_path, TRACKING_PATH ))
+    calib = Calibration(os.path.join(pkg_path, CALIB_PATH,), from_video=True)
 
     rate = rospy.Rate(10)
     while not rospy.is_shutdown():
-        boxes = np.array(df_tracking[df_tracking['frame']==frame][['bbox_left', 'bbox_top', 'bbox_right', 'bbox_bottom']])
-        types = np.array(df_tracking[df_tracking['frame']==frame]['type'])
-        img = read_camera(os.path.join(DATA_PATH, 'image_02/data/%010d.png'%frame))
-        point_cloud = read_point_cloud(os.path.join(DATA_PATH, 'velodyne_points/data/%010d.bin'%frame))
-        imu_data = read_imu(os.path.join(DATA_PATH, 'oxts/data/%010d.txt'%frame))
-        publish_camera(cam_pub, bridge, img, boxes, types)
+        df_tracking_frame = df_tracking[df_tracking.frame==frame]
+
+        boxes_2d = np.array(df_tracking_frame[['bbox_left', 'bbox_top', 'bbox_right', 'bbox_bottom']])
+        types = np.array(df_tracking_frame['type'])
+        boxes_3d = np.array(df_tracking_frame[['height', 'width', 'length', 'pos_x', 'pos_y', 'pos_z', 'rot_y']])
+        track_ids =  np.array(df_tracking_frame['track_id'])
+
+        corners_3d_velos = []
+        for box_3d in boxes_3d:
+            # use * to flatten box_3d array
+            corners_3d_cam2 = compute_3d_box_cam2(*box_3d)
+            corners_3d_velo = calib.project_rect_to_velo(corners_3d_cam2.T)
+            corners_3d_velos += [corners_3d_velo]
+
+        img = read_camera(os.path.join(pkg_path, DATA_PATH, 'image_02/data/%010d.png'%frame))
+        point_cloud = read_point_cloud(os.path.join(pkg_path, DATA_PATH, 'velodyne_points/data/%010d.bin'%frame))
+        imu_data = read_imu(os.path.join(pkg_path, DATA_PATH, 'oxts/data/%010d.txt'%frame))
+        
+        publish_camera(cam_pub, bridge, img, boxes_2d, types)
         publish_point_cloud(pcl_pub, point_cloud)
         publish_imu(imu_pub, imu_data)
         publish_ego_car(ego_pub)
+        publish_3dbox(box3d_pub, corners_3d_velos, track_ids, types)
         publish_gps(gps_pub, imu_data)
         rospy.loginfo('published')
         rate.sleep()
--- catkin_ws/src/kitti_tutorial/script/publish_utils.py
+++ catkin_ws/src/kitti_tutorial/script/publish_utils.py
@@ -14,6 +14,14 @@ import tf
 FRAME_ID = 'map'
 DETECTION_COLOR_DICT = {'Car':(255, 255, 0), 'Pedestrian':(0, 226, 255), 'Cyclist':(141, 40, 255)}
 
+LIFETIME = 0.1
+LINES = [[0, 1], [1, 2], [2, 3], [3, 0]] # lower face
+LINES+= [[4, 5], [5, 6], [6, 7], [7, 4]] # upper face
+LINES+= [[4, 0], [5, 1], [6, 2], [7, 3]] # connect lower face and upper face
+LINES+= [[4, 1], [5, 0]] # front face
+
+Q_ZERO = tf.transformations.quaternion_from_euler(0, 0, 0)
+
 def publish_camera(cam_pub, bridge, image, boxes, types):
     for box, data_type in zip(boxes, types):
         top_left = int(box[0]), int(box[1])
@@ -50,11 +58,10 @@ def publish_ego_car(ego_car_pub):
     marker.color.b = 0.0
     marker.color.a = 1.0
     marker.scale.x = 0.2
-    q = tf.transformations.quaternion_from_euler(0, 0, 0)
-    marker.pose.orientation.x = q[0]
-    marker.pose.orientation.y = q[1]
-    marker.pose.orientation.z = q[2]
-    marker.pose.orientation.w = q[3]
+    marker.pose.orientation.x = Q_ZERO[0]
+    marker.pose.orientation.y = Q_ZERO[1]
+    marker.pose.orientation.z = Q_ZERO[2]
+    marker.pose.orientation.w = Q_ZERO[3]
 
     marker.points = []
     marker.points.append(Point(10, -10, 0))
@@ -133,3 +140,93 @@ def publish_gps(gps_pub, gps_data, log=False):
     gps_pub.publish(gps)
     if log:
         rospy.loginfo("gps msg published")
+
+def publish_3dbox(box3d_pub, corners_3d_velos, track_ids, object_types=None, \
+    publish_id=True, publish_distance=False, log=False):
+    """
+    Publish 3d boxes in velodyne coordinate, with color specified by object_types
+    If object_types is None, set all color to cyan
+    corners_3d_velos : list of (8, 4) 3d corners
+    """
+    marker_array = MarkerArray()
+    for i, corners_3d_velo in enumerate(corners_3d_velos):
+        track_id = track_ids[i]
+        marker = Marker()
+        marker.header.frame_id = FRAME_ID
+        marker.header.stamp = rospy.Time.now()
+
+        # marker.id = track_id
+        marker.id = i
+        marker.action = Marker.ADD
+        marker.lifetime = rospy.Duration(LIFETIME)
+        marker.type = Marker.LINE_LIST
+
+        if object_types is None:
+            marker.color.r = 0.0
+            marker.color.g = 1.0
+            marker.color.b = 1.0
+        else:
+            b, g, r = DETECTION_COLOR_DICT[object_types[i]]
+            marker.color.r = r/255.0
+            marker.color.g = g/255.0
+            marker.color.b = b/255.0
+
+        marker.color.a = 1.0
+        marker.scale.x = 0.2
+        marker.pose.orientation.x = Q_ZERO[0]
+        marker.pose.orientation.y = Q_ZERO[1]
+        marker.pose.orientation.z = Q_ZERO[2]
+        marker.pose.orientation.w = Q_ZERO[3]
+
+        marker.points = []
+        for l in LINES:
+            p1 = corners_3d_velo[l[0]]
+            marker.points.append(Point(p1[0], p1[1], p1[2]))
+            p2 = corners_3d_velo[l[1]]
+            marker.points.append(Point(p2[0], p2[1], p2[2]))
+        marker_array.markers.append(marker)
+
+        if publish_id:
+            text_marker = Marker()
+            text_marker.header.frame_id = FRAME_ID
+            text_marker.header.stamp = rospy.Time.now()
+
+        # separate from previous only with i
+        text_marker.id = i + 1000
+        text_marker.action = Marker.ADD
+        text_marker.lifetime = rospy.Duration(LIFETIME)
+        text_marker.type = Marker.TEXT_VIEW_FACING
+
+        # assign the position of the marker
+        p4 = corners_3d_velo[4] # upper front left corner
+
+        text_marker.pose.position.x = p4[0]
+        text_marker.pose.position.y = p4[1]
+        text_marker.pose.position.z = p4[2] + 0.5
+
+        text_marker.pose.orientation.x = Q_ZERO[0]
+        text_marker.pose.orientation.y = Q_ZERO[1]
+        text_marker.pose.orientation.z = Q_ZERO[2]
+        text_marker.pose.orientation.w = Q_ZERO[3]
+
+        # text_marker.text = str(track_id)
+        text_marker.text = str(track_ids[i])
+
+
+        text_marker.scale.x = 1
+        text_marker.scale.y = 1
+        text_marker.scale.z = 1
+
+        if object_types is None:
+            text_marker.color.r = 0.0
+            text_marker.color.g = 1.0
+            text_marker.color.b = 1.0
+        else:
+            b, g, r = DETECTION_COLOR_DICT[object_types[i]]
+            text_marker.color.r = r/255.0
+            text_marker.color.g = g/255.0
+            text_marker.color.b = b/255.0
+        text_marker.color.a = 1.0
+        marker_array.markers.append(text_marker)
+
+    box3d_pub.publish(marker_array)
